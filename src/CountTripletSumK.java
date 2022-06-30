09d634cb19ef51f20862f358ff1a7d29f45245f5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 22:48:55 -0700

    added the annotations for the better understandings

--- src/CountTripletSumK.java
+++ src/CountTripletSumK.java
@@ -1,12 +1,23 @@
+import java.util.Arrays;
+
 public class CountTripletSumK {
+   /*count the number of triplets which results the sum of two numbers equals the third
+   yes lets begin
+    */
     public static int countTripletsWithSumK(int[] arr, int n){
+        //sort array first
+        Arrays.sort(arr);
         int i, j, k;
         int count = 0;
+        // take a pointer from end
         for(k= n-1; k> 0; k--){
+            //one from the start and other from the second last
             i=0;
             j= k-1;
+            //until the both points meet
             while(j<i){
                 if(arr[i]+arr[j] == arr[k]){
+                    //if yes increment the count and the pointers respectively
                     count ++;
                     i++;
                     j--;
@@ -15,7 +26,7 @@ public class CountTripletSumK {
                 }
                 else{
                     j--;
-                }
+                }//after the while loop, will go for another iteration with k... and so on
             }
         }return count;
     }
