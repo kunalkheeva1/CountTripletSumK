ba554653ed9b0be658b4d219e622ea4fae9c48ea
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 22:37:16 -0700

    added a method to count the sum of i and j = k

--- src/CountTripletSumK.java
+++ src/CountTripletSumK.java
@@ -1,4 +1,25 @@
 public class CountTripletSumK {
+    public static int countTripletsWithSumK(int[] arr, int n){
+        int i, j, k;
+        int count = 0;
+        for(k= n-1; k> 0; k--){
+            i=0;
+            j= k-1;
+            while(j<i){
+                if(arr[i]+arr[j] == arr[k]){
+                    count ++;
+                    i++;
+                    j--;
+                }else if(arr[i] +arr[j]< arr[k]){
+                    i++;
+                }
+                else{
+                    j--;
+                }
+            }
+        }return count;
+    }
+
     public static void main(String[] args) {
 
     }
